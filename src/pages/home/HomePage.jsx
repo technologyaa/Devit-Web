fcac2ed861359df480bd219a984cd668794d929c
장강민 <jkm9780533@gmail.com>
2025-10-21 21:44:09 +0900

    feat: 홈 화면 UI Top부분 구현

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -6,10 +6,20 @@ export default function HomePage() {
     <>
       <Helmet>
         <title>Devit</title>
-        <link rel="icon" href="./assets/Helmet.svg"></link>
+        <link rel="icon" href="/assets/devit-logo(Di).png"></link>
       </Helmet>
       <S.Container>
-        <S.Frame></S.Frame>
+        <S.Frame>
+          <S.Topmiddlewrap>
+            <S.Top>
+              <S.Name><span style={{ color: "#883CBE" }}>개발</span>과 <span style={{ color: "#D4AAF3" }}>기획</span>을 잇다</S.Name>
+              <S.Cricle1></S.Cricle1>
+              <S.Cricle2></S.Cricle2>
+            </S.Top>
+            <S.Middle></S.Middle>
+          </S.Topmiddlewrap>
+          <S.Bottom></S.Bottom>
+        </S.Frame>
       </S.Container>
     </>
   );
--- src/pages/home/styles/homePage.js
+++ src/pages/home/styles/homePage.js
@@ -10,6 +10,56 @@ export const Container = styled.div`
 
 export const Frame = styled.div`
   width: 88%;
-  height: 93%;
-  border: 1px solid #ccc;
+  height: 94%;
+  display: flex;
+  flex-direction: column;
+  gap: 55px;
+`;
+
+export const Topmiddlewrap = styled.div``;
+
+export const Top = styled.div`
+  width: 100%;
+  height: 160px;
+  position: relative;
+  overflow: hidden;
+`;
+
+export const Middle = styled.div`
+  width: 100%;
+  height: 170px;
+  background-color: gray;
+`;
+
+export const Bottom = styled.div`
+  width: 100%;
+  height: 320px;
+  background-color: green;
+`;
+
+export const Name = styled.div`
+  font-size: 50px;
+  font-weight: 600;
+  position: absolute;
+  top: 30px;
+`;
+
+export const Cricle1 = styled.div`
+  position: absolute;
+  top: -145px;
+  right: 50px;
+  background-color: rgba(171, 102, 221, 0.7);
+  width: 250px;
+  height: 250px;
+  border-radius: 100%;
+`;
+
+export const Cricle2 = styled.div`
+  position: absolute;
+  top: -200px;
+  right: -130px;
+  background-color: rgba(136, 60, 190, 0.4);
+  width: 340px;
+  height: 340px;
+  border-radius: 100%;
 `;
