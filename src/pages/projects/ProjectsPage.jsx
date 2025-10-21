26fb5850a3f5238ca7839c9517eb2411f3aaaa02
한승환 <coderrornotfound@gmail.com>
2025-10-21 23:28:38 +0900

    feat: 프로젝트 UI 구현

--- src/pages/projects/ProjectsPage.jsx
+++ src/pages/projects/ProjectsPage.jsx
@@ -1,15 +1,64 @@
 import * as S from "./styles/projectsPage";
 import { Helmet } from "react-helmet";
 
+const projectList = [
+  {
+    id: 1,
+    title: "Devit",
+    owner: "강장민",
+    thumbnail: "/assets/dummy-thumbnail.svg",
+  },
+  {
+    id: 1,
+    title: "Devit",
+    owner: "강장민",
+    thumbnail: "/assets/dummy-thumbnail.svg",
+  },
+  {
+    id: 1,
+    title: "Devit",
+    owner: "강장민",
+    thumbnail: "/assets/dummy-thumbnail.svg",
+  },
+  {
+    id: 1,
+    title: "Devit",
+    owner: "강장민",
+    thumbnail: "/assets/dummy-thumbnail.svg",
+  },
+  {
+    id: 1,
+    title: "Devit",
+    owner: "강장민",
+    thumbnail: "/assets/dummy-thumbnail.svg",
+  },
+];
+
 export default function ProjectsPage() {
   return (
     <>
       <Helmet>
         <title>Devit</title>
-        <link rel="icon" href="./assets/Helmet.svg"></link>
+        <link rel="icon" href="./assets/Helmet.svg" />
       </Helmet>
       <S.Container>
-        <S.Frame></S.Frame>
+        <S.Frame>
+          <S.Top>프로젝트</S.Top>
+          <S.Bottom>
+            {projectList.map((project) => (
+              <S.Box key={project.id}>
+                <S.Thumbnail
+                  src={project.thumbnail}
+                  alt={`${project.title} 썸네일`}
+                />
+                <S.BoxBottom>
+                  <S.Title>{project.title}</S.Title>
+                  <S.Owner>{project.owner}</S.Owner>
+                </S.BoxBottom>
+              </S.Box>
+            ))}
+          </S.Bottom>
+        </S.Frame>
       </S.Container>
     </>
   );
--- src/pages/projects/styles/projectsPage.js
+++ src/pages/projects/styles/projectsPage.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Image } from "@/styles/Image";
 
 export const Container = styled.div`
   width: 100%;
@@ -11,5 +12,54 @@ export const Container = styled.div`
 export const Frame = styled.div`
   width: 88%;
   height: 93%;
-  border: 1px solid #ccc;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+export const Top = styled.div`
+  font-size: 26px;
+  font-weight: 500;
+  color: #111;
+`;
+
+export const Bottom = styled.div`
+  display: flex;
+  gap: 24px;
+  flex-wrap: wrap;
+`;
+
+export const Box = styled.div`
+  width: 260px;
+  height: 200px;
+  background-color: #fff;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  cursor: pointer;
+  gap: 4px;
+`;
+
+export const Thumbnail = styled(Image)`
+  width: 100%;
+  height: 160px;
+  border-radius: 18px;
+  object-fit: cover;
+`;
+
+export const BoxBottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Title = styled.div`
+  font-size: 20px;
+  font-weight: 500;
+  color: #000;
+`;
+
+export const Owner = styled.div`
+  font-size: 16px;
+  color: #000;
 `;
