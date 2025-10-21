0d8a463913840c127a2b78d9f89742a7a3080db0
한승환 <coderrornotfound@gmail.com>
2025-10-21 19:40:17 +0900

    fix: 사이드바 넓이 자연스럽게 구현

--- src/widgets/layout/SideBar/styles/sideBarFolded.js
+++ src/widgets/layout/SideBar/styles/sideBarFolded.js
@@ -2,7 +2,7 @@ import { Image } from "@/styles/Image";
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 80px;
+  width: 82px;
   height: 100vh;
   border-right: 1px solid #d9dce0;
   display: flex;
