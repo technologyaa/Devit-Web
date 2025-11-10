45edf7c7f676bf4b1ebd8a85b6b3a160c3c111eb
한승환 <coderrornotfound@gmail.com>
2025-11-11 08:35:28 +0900

    fix: border 튀어나오는 것 수정

--- src/widgets/layout/SideBar/styles/sideBarFolded.js
+++ src/widgets/layout/SideBar/styles/sideBarFolded.js
@@ -2,12 +2,13 @@ import { Image } from "@/styles/Image";
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 82px;
+  width: 100%;
   height: 100vh;
   border-right: 1px solid #d9dce0;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  box-sizing: border-box;
 `;
 
 export const Top = styled.div`
