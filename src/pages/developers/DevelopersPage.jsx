7a569647298ff281351d291128d74d0ba7016c1f
장강민 <jkm9780533@gmail.com>
2025-11-11 23:15:28 +0900

    feat: 개발자 목록 ui 구현

--- src/pages/developers/DevelopersPage.jsx
+++ src/pages/developers/DevelopersPage.jsx
@@ -1,6 +1,8 @@
 import * as S from "./styles/developersPage";
 import { Helmet } from "react-helmet";
 import { useState } from "react";
+import { Alarm } from "@/toasts/Alarm";
+import users from "@/data/user-list";
 
 // 직무 카테고리
 const CATEGORIES = [
@@ -13,6 +15,14 @@ const CATEGORIES = [
   "디자인",
 ];
 
+const truncateText = (text, maxLength = 15) => {
+  if (text.length > maxLength) {
+    // 15글자까지 자르고 '...'을 추가합니다.
+    return text.substring(0, maxLength) + '...';
+  }
+  return text;
+};
+
 export default function DevelopersPage() {
 
   const [selectedCategory, setSelectedCategory] = useState("전체");
@@ -22,7 +32,7 @@ export default function DevelopersPage() {
     // 여기서 선택된 카테고리에 따른 데이터 필터링 로직을 추가할 수 있습니다.
     console.log(`Selected category: ${category}`);
   };
-  
+
   return (
     <>
       <Helmet>
@@ -31,35 +41,55 @@ export default function DevelopersPage() {
       </Helmet>
       <S.Container>
         <S.Frame>
-          <S.DevText>개발자</S.DevText>
-          <S.DevSearch>
-            <S.SearchBox>
-              <S.SearchIcon src="/public/assets/Search.svg"></S.SearchIcon>
-              <S.Search type="search" placeholder="검색어를 입력해주세요."></S.Search>
-            </S.SearchBox>
-          </S.DevSearch>
-          <S.Category>
-            <S.Position>
-            {CATEGORIES.map((category) => (
-              <S.CategoryButton
-                key={category}
-                // $active prop으로 선택 상태 전달 (styled-components에서 DOM 요소로 전달되지 않도록 $를 붙임)
-                $active={selectedCategory === category}
-                onClick={() => handleCategoryClick(category)}
-              >
-                {category}
-              </S.CategoryButton>
-            ))}
-          </S.Position>
-          
-          <S.FilterGroup>
-                <S.DropdownButton>
-                    전체 경력 <span style={{fontSize: '10px'}}>▼</span>
-                </S.DropdownButton>
-          </S.FilterGroup>
+          <S.Top>
+            <S.TextSearch>
+              <S.DevText>개발자</S.DevText>
+              <S.DevSearch>
+                <S.SearchBox>
+                  <S.SearchIcon src="/public/assets/Search.svg"></S.SearchIcon>
+                  <S.Search type="search" placeholder="검색어를 입력해주세요."></S.Search>
+                </S.SearchBox>
+              </S.DevSearch>
+            </S.TextSearch>
 
-          </S.Category>
-          
+            <S.Category>
+              <S.Position>
+                {CATEGORIES.map((category) => (
+                  <S.CategoryButton
+                    key={category}
+                    // $active prop으로 선택 상태 전달 (styled-components에서 DOM 요소로 전달되지 않도록 $를 붙임)
+                    $active={selectedCategory === category}
+                    onClick={() => handleCategoryClick(category)}
+                  >
+                    {category}
+                  </S.CategoryButton>
+                ))}
+              </S.Position>
+
+              <S.FilterGroup onClick={() => Alarm("🛠️", "아직 개발중인 기능입니다.")}>
+                <S.DropdownButton>
+                  전체 경력 <span style={{ fontSize: '10px' }}>▼</span>
+                </S.DropdownButton>
+              </S.FilterGroup>
+            </S.Category>
+          </S.Top>
+
+          <S.DevUser>
+            {users.map((user) => (
+              <S.DeveloperCard key={user.id}>
+                <S.ProfileArea>
+                  <S.TemperatureBar $temp={user.temp} />
+                  <S.ProfileImg src={user.img}></S.ProfileImg>
+                </S.ProfileArea>
+
+                <S.CardInfoArea>
+                  <S.CardName>{user.name}</S.CardName>
+                  <S.CardJob>{user.job}</S.CardJob>
+                  <S.CardInfo>{truncateText(user.info, 15)}</S.CardInfo>
+                </S.CardInfoArea>
+              </S.DeveloperCard>
+            ))}
+          </S.DevUser>
         </S.Frame>
       </S.Container>
     </>
