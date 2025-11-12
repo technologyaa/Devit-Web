be460a5172b86fd8216b1ec77c5ed47a9a0c7e1e
장강민 <jkm9780533@gmail.com>
2025-11-12 10:58:26 +0900

    fix: SearchBox 아이콘 안나타남 수정 test

--- src/pages/developers/DevelopersPage.jsx
+++ src/pages/developers/DevelopersPage.jsx
@@ -64,7 +64,7 @@ export default function DevelopersPage() {
               <S.DevText>개발자</S.DevText>
               <S.DevSearch>
                 <S.SearchBox>
-                  <S.SearchIcon src="/public/assets/Search.svg"></S.SearchIcon>
+                  <S.SearchIcon src="/assets/Search.svg"></S.SearchIcon>
                   <S.Search type="search" placeholder="검색어를 입력해주세요."></S.Search>
                 </S.SearchBox>
               </S.DevSearch>
