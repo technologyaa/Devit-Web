0b4250aaccbd9c07774ab6e82d4d26eadb059ddc
한승환 <coderrornotfound@gmail.com>
2025-11-12 20:54:21 +0900

    fix: 사이드바 접힐 때 자연스럽게 수정

--- src/widgets/layout/Layout/styles/layout.js
+++ src/widgets/layout/Layout/styles/layout.js
@@ -6,7 +6,7 @@ export const Wrapper = styled.div`
   display: grid;
   grid-template-columns: ${(props) =>
     props.isChatPage
-      ? "80px 1fr"
+      ? "82px 1fr"
       : "240px 1fr"}; // 사이드바 접힘 여부에 따라 변경
 `;
 
